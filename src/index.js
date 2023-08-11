118021fa4b7242ce09fa8f56e8170e4d2e508205
Lurie <lurainekimmerle@gmail.com>
2023-08-11 06:00:35 +0000

    added content to <li>

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ function Page() {
       </header>
       <h1>Why I'm excited to learn React!</h1>
       <ol>
-        <li>reason 1</li>
-        <li>reason 2</li>
+        <li>It allows me to build cool things</li>
+        <li>It is imparative</li>
       </ol>
       <footer>
         <small>© 2023 Kimmerle development. All rights reserved.</small>
