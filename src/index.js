add02fac3acecef7a6b63c6889a2df7309e6be67
Lurie Kimmerle <lurainekimmerle@gmail.com>
2023-08-11 13:51:33 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function Page() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <img alt="logo img" src="React-icon.png" width="40%" />
+        </nav>
+      </header>
+      <h1>Why I'm excited to learn React!</h1>
+      <ol>
+        <li>reason 1</li>
+        <li>reason 2</li>
+      </ol>
+      <footer>© 2023 Kimmerle development. All rights reserved.</footer>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Page />);
